9be0bfe9682fc41a9bed617d79f739c2793c419d
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2022-04-21 17:18:52 +0300

    profile moderator

---
+++ src/blocks/modules/moderator/moderator-messenger/moderator-messenger.js
@@ -0,0 +1,13 @@
+modules.define('moderator-messenger', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
