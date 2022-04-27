c822c01e02e64b9a910e09eec0112f6d14a5c469
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2022-04-27 17:15:08 +0300

    add film

--- dist/js/moderator.js
+++ dist/js/moderator.js
@@ -65,5 +65,50 @@ document.addEventListener('DOMContentLoaded', function (event) {
         })
     })
 
+    /* =====================================
+    sendFiles
+    =====================================*/
+
+    function sendFiles(files, elem, callback) {
+
+
+        for (var i = 0; i < files.length; i++) {
+            var file = files.item(i);
+
+            // проверяем размер файла
+            if (file.size > 1200000) {
+                alert('Размер файла не должен превышать 1 мб')
+                return false;
+            }
+
+            if (file.type === 'image/jpeg') {
+
+                var reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = function (e) {
+                    callback(e.target.result)
+                }
+
+            } else {
+                alert('Допустимы только JPEG изображения ');
+            }
+
+        }
+    }
+
+    document.querySelector('[data-attach=poster]').addEventListener('change', function () {
+
+        let files = this.files;
+        let elem = this;
+
+        sendFiles(files, elem, function (dataImage) {
+
+            elem.closest('.film-poster__cover').classList.add('cover--loaded')
+            elem.closest('.film-poster__cover').querySelector('[data-attach="preview-poster"]').src = dataImage
+
+        });
+
+    })
+
 
 });
