7f8137a520856bfec7e337f1f2f429c6124d91ad
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2022-04-29 08:42:29 +0300

    page add film

--- src/js/common/moderator.js
+++ src/js/common/moderator.js
@@ -111,4 +111,88 @@ document.addEventListener('DOMContentLoaded', function (event) {
     })
 
 
+    /* =============================================
+    repeeter
+    ============================================= */
+
+    if (document.querySelector('[data-repeeat="add"]')) {
+        document.querySelector('[data-repeeat="add"]').addEventListener('click', function (event) {
+            const container = document.querySelector('[data-repeeat="container"]')
+            const fieldRepeeat = container.children[0].cloneNode(true)
+            const lastElem = fieldRepeeat.children[(fieldRepeeat.children.length - 1)]
+
+            //max 10 fields
+            if (container.querySelectorAll('.remove-repeeatter').length > 9) {
+                window.STATUS.err('Допустимо не более 10 элементов')
+
+                return false;
+            }
+
+            //remove disabled attr
+            lastElem.querySelector('input').removeAttribute('disabled')
+            lastElem.querySelector('input').setAttribute('placeholder', 'Должность')
+            lastElem.querySelector('input').setAttribute('value', '')
+
+            //create remove button
+            const removeElem = document.createElement('span')
+            removeElem.classList.add('remove-repeeatter')
+
+            //remove string
+            removeElem.addEventListener('click', function (event) {
+
+                if (confirm('Удалить?')) {
+                    event.target.closest('.form__item').remove()
+                }
+
+            })
+
+
+            //append elem
+            lastElem.append(removeElem)
+            container.append(fieldRepeeat)
+
+
+        })
+
+
+
+
+    }
+
+    /* =================================================
+    timepicker
+    =================================================*/
+
+    if (document.querySelector('[data-timepicker]')) {
+
+        document.querySelectorAll('[data-timepicker]').forEach(function (item) {
+
+
+            item.addEventListener('click', function (event) {
+                console.log(item)
+            })
+
+            //add event
+            // item.addEventListener('click', function () {
+
+            //     console.log('inMapPopup')
+
+            //     var inMapPopup = new customModal()
+
+            //     console.log(inMapPopup)
+
+
+            //     inMapPopup.open('<div id="af-timepicker">timepicker</div>', function (instanse) {
+
+            //     })
+            // })
+
+        })
+
+    }
+
+
+
+
+
 });
